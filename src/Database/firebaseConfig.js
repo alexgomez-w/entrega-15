a1033fbe0eaab055e1e71e6d3f0bfa91800b04a9
Alexander Gomez <alexander.gomez@wundermanthompson.com>
2023-08-10 22:53:26 -0300

    entrega

---
+++ src/Database/firebaseConfig.js
@@ -0,0 +1,5 @@
+export const realtime_database_url = 'https://test-ecom-c880e-default-rtdb.firebaseio.com/'
+
+export const api_key = 'AIzaSyALO6QtekRSVuPalyeicWC2fb_ihWn1HcA'
+
+export const google_maps_api_key = 'AIzaSyAZBlkaRoqnBTkUxbcavyT4YLueYhmL6JI'
